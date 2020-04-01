93f3be8aac041575064095e94397d77de4193e0a
darkfantasy88 <59861596+darkfantasy88@users.noreply.github.com>
2020-03-31 23:43:05 -0500

    HTML Stuff

--- PrisonManagementSystem/WebApi_v1/Controllers/MedicalController.cs
+++ PrisonManagementSystem/WebApi_v1/Controllers/MedicalController.cs
@@ -1,32 +1,63 @@
-﻿using System;
+﻿using ManagementLibrary.Api.Models.Medical;
+using ManagementLibrary.Sql._Internal._SqlDataAccess._Medical;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApi_v1.Controllers
 {
+    [EnableCors("*","*","*")]
+    [RoutePrefix("api/medical")]
     public class MedicalController : ApiController
     {
-        // GET: api/Medical
-        public IEnumerable<string> Get()
+        IMedicalDataAccess _medical;
+        public MedicalController(IMedicalDataAccess medicial)
         {
-            return new string[] { "value1", "value2" };
+            _medical = medicial;
+        }
+        //List<Person> people = new List<Person>()
+        //{
+        //    new Person(){FirstName="Rohan",LastName="Cooper",
+        //        Age =20,Occupation="Student"},
+        //    new Person(){FirstName="Avianna",LastName="Williams",
+        //        Age =20,Occupation="Student"},
+        //    new Person(){FirstName="Toni-Ann",LastName="Wright",
+        //        Age =20,Occupation="Student"},
+        //    new Person(){FirstName="Lilli-Ann",LastName="Longmore",
+        //        Age =20,Occupation="Student"}
+        //};
+        // GET: api/Medical
+        [Route("getall")]
+        [HttpGet]
+        public IEnumerable<IMedicial> Get()
+        {
+            return _medical.GetMedical();
         }
 
         // GET: api/Medical/5
-        public string Get(int id)
+        [Route("getperson/{firstname}/{lastname}")]
+        [HttpGet]
+        public IMedicial Get(string firstname,string lastname)
         {
-            return "value";
+            return null;
+            //return people.Where(x=> x.FirstName==firstname && x.LastName==lastname).FirstOrDefault();
         }
 
         // POST: api/Medical
+        [HttpPost]
+        [Route("insertperson")]
+        //This must change
         public void Post([FromBody]string value)
         {
+            //people.Add(value);
         }
 
         // PUT: api/Medical/5
+        //This mmust change
         public void Put(int id, [FromBody]string value)
         {
         }
@@ -36,4 +67,13 @@ namespace WebApi_v1.Controllers
         {
         }
     }
+
+    /*public class Person 
+    {
+      public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Occupation { get; set; }
+
+    }*/
 }
